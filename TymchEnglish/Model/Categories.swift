0a9f1fd473da8bed31fc5e59980ebbab0d1f4495
thoughtcrime <arahnoid911@gmail.com>
2024-02-02 21:05:22 -0500

    8th of April

--- TymchEnglish/Model/Categories.swift
+++ TymchEnglish/Model/Categories.swift
@@ -40,17 +40,17 @@ struct PoemCategories: Hashable {
     static let sampleCategory = PoemCategory(category: .animals, title: "Poems and songs about animals and insects", categoryImage: "imagePlaceholder")
     
     static let categories = [
-        PoemCategory(category: .animals, title: "Poems and songs about animals and insects", categoryImage: "imagePlaceholder"),
-        PoemCategory(category: .actions, title: "Action songs and rhymes", categoryImage: "imagePlaceholder"),
-        PoemCategory(category: .bodyParts, title: "Poems and songs about body parts", categoryImage: "imagePlaceholder"),
-        PoemCategory(category: .dailyRoutines, title: "Poems and songs about daily routines", categoryImage: "imagePlaceholder"),
-        PoemCategory(category: .colors, title: "Poems and songs about colors", categoryImage: "imagePlaceholder"),
-        PoemCategory(category: .emotions, title: "Poems and songs about emotions", categoryImage: "imagePlaceholder"),
-        PoemCategory(category: .themed, title: "Themed songs and poems", categoryImage: "imagePlaceholder"),
-        PoemCategory(category: .lullabye, title: "Lullabyes", categoryImage: "imagePlaceholder"),
-        PoemCategory(category: .parachute, title: "Parachute and scarf songs and rhymes", categoryImage: "imagePlaceholder"),
-        PoemCategory(category: .transportation, title: "Poems and songs about transportation", categoryImage: "imagePlaceholder"),
-        PoemCategory(category: .seasons, title: "Poems and songs about seasongs and weather", categoryImage: "imagePlaceholder")
+        PoemCategory(category: .animals, title: "Poems and songs about animals and insects", categoryImage: "animals"),
+        PoemCategory(category: .actions, title: "Action songs and rhymes", categoryImage: "action"),
+        PoemCategory(category: .bodyParts, title: "Poems and songs about body parts", categoryImage: "body"),
+        PoemCategory(category: .dailyRoutines, title: "Poems and songs about daily routines", categoryImage: "routines"),
+        PoemCategory(category: .colors, title: "Poems and songs about colors", categoryImage: "colors"),
+        PoemCategory(category: .emotions, title: "Poems and songs about emotions", categoryImage: "emotions"),
+        PoemCategory(category: .themed, title: "Themed songs and poems", categoryImage: "themed"),
+        PoemCategory(category: .lullabye, title: "Lullabyes", categoryImage: "lulliby"),
+        PoemCategory(category: .parachute, title: "Parachute and scarf songs and rhymes", categoryImage: "scarf"),
+        PoemCategory(category: .transportation, title: "Poems and songs about transportation", categoryImage: "transport"),
+        PoemCategory(category: .seasons, title: "Poems and songs about seasongs and weather", categoryImage: "seasons")
     ]
 }
 
